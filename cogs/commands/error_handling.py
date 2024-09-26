e17d1ac715b26dc3347d00dea2e53d12ada8b0e2
Faauli <148459442+Faauli@users.noreply.github.com>
2024-09-26 13:08:21 +0200

    Update and rename error_handler.py to error_handling.py

---
+++ cogs/commands/error_handling.py
@@ -0,0 +1,24 @@
+# Imports
+import discord
+from discord.ext import commands
+from discord.commands import slash_command
+
+
+class Base(commands.Cog):
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
+
+    #Slash Command
+    @slash_command(description="command3")
+    async def hello(self, ctx: discord.ApplicationContext):
+        member = ctx.author
+        try:
+            await member.edit(nick="Nickname")
+        except discord.Forbidden:
+            await ctx.respond("I do not have permission to edit the nickname")
+            return
+        await ctx.respond("The nickname has been edited")
+
+
+def setup(bot: discord.Bot):
+    bot.add_cog(Base(bot))
